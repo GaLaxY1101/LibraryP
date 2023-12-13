edded4a696ede892aad9ff2f08e1c2f9d937ea20
Oleksandr Zborivskiy <ozborivskiy@gmail.com>
2023-12-13 22:03:04 +0200

    Create test for Book class

--- BookManager.Tests/BookTests.cs
+++ BookManager.Tests/BookTests.cs
@@ -9,6 +9,50 @@ namespace BookManager.Tests
 {
     public class BookTests
     {
-       
+        [Fact]
+        public void Equals_InstanceWithTheSameFieldsAreEquals_ReturnTrue()
+        {
+            //Arrange 
+            var sut = new Book("Harry Potter", "Joahn Rowling", "Fantasy");
+            
+            var expected = true;
+            
+            //Act
+
+            var actual = sut.Equals(new Book("Harry Potter", "Joahn Rowling", 		"Fantasy"));
+
+            //Arrange
+            Assert.Equal(expected, actual);
+        
+        }
+
+        [Fact]
+        public void Equals_InstancesWithDifferentFieldsAreNotEquals_ReturnFalse()
+        {
+            //Arrange
+            var sut = new Book("SomeTitle", "SomeAuthor", "Genre");
+
+            var expected = false;
+
+            //Act
+            var actual = sut.Equals(new Book("Title", "Author", "Genre"));
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToString_CorrectToStringFormat()
+        {
+            //Arrange
+            var sut = new Book("Harry Potter", "Joahn Rowling", "Fantasy");
+            var expected = "Harry Potter by Joahn Rowling. Fantasy";
+            
+            //Act
+            var actual = sut.ToString();
+
+            //Assert 
+            Assert.Equal(expected, actual);
+        }
     }
 }
