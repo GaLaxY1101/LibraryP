7c84cc0c960289f45b18b247500ce7a94f64bc87
Illia Korniychik <korniykhik3@gmail.com>
2023-12-14 00:54:43 +0200

    implement class Book and BookManager

--- BookManagerProject/Book.cs
+++ BookManagerProject/Book.cs
@@ -8,6 +8,30 @@ namespace BookManagerProject
 {
     public class Book
     {
-       
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+
+        public Book() { }
+
+        public Book(string title, string author, string genre)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} by {Author}. {Genre}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if (obj is not Book) { return false; }
+
+            return ((Title.Equals(((Book)obj).Title)) && (Author.Equals(((Book)obj).Author)) && (Genre.Equals(((Book)obj).Genre)));
+        }
     }
 }
--- BookManagerProject/BookManager.cs
+++ BookManagerProject/BookManager.cs
@@ -8,6 +8,77 @@ namespace BookManagerProject
 {
     public class BookManager
     {
-        
+        private HashSet<string> genres = new HashSet<string>();
+        private List<Book> books = new List<Book>();
+        private HashSet<Book> favorites = new HashSet<Book>();
+
+        public void AddGenre(string genre)
+        {
+            genres.Add(genre);
+        }
+
+        public HashSet<string> GetGenres() => new HashSet<string>(genres);
+
+        public void RemoveGenre(string genre)
+        {
+            genres.Remove(genre);
+        }
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public void RemoveBook(string title)
+        {
+            books.RemoveAll(book => book.Title == title);
+        }
+
+        public void AddToFavorites(string title)
+        {
+            if (books.Any(book => book.Title == title))
+            {
+                favorites.Add(books.Where(book => book.Title == title).FirstOrDefault());
+            }
+            else
+            {
+                throw new ArgumentException($"There is no book called {title} in the list.");
+            }
+        }
+
+        public void RemoveFromFavorites(string title)
+        {
+            if (favorites.Any(book => book.Title == title))
+            {
+                favorites.Remove(favorites.Where(book => book.Title == title).FirstOrDefault());
+            }
+            else
+            {
+                throw new ArgumentException($"There is no book called {title} in the list.");
+            }
+        }
+
+        public List<Book> GetBooks()
+        {
+            return new List<Book>(books);
+        }
+
+        public List<Book> GetFavorites()
+        {
+            return new List<Book>(favorites);
+        }
+
+        public List<Book> GetSortedFavorites()
+        {
+            return favorites.OrderByDescending(book => book.Title).ToList();
+        }
+
+        public List<Book> GetBooksByGenre(string genre)
+        {
+            return books
+                .Where(book => book.Genre == genre)
+                .OrderByDescending(book => book.Title)
+                .ToList();
+        }
     }
 }
