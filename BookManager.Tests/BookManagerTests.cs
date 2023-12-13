46da6b3a451b2e2ba71017c874ad1dc58708c469
Daria Putro <dashkaputro@gmail.com>
2023-12-13 22:32:12 +0200

    Add a few tests for addBook, removeBook and AddGenre

--- BookManager.Tests/BookManagerTests.cs
+++ BookManager.Tests/BookManagerTests.cs
@@ -5,9 +5,54 @@ using System;
 namespace BookManager.Tests
 {
     public class BookManagerTests
-    { 
+    {
+        [Fact]
+        public void AddGenre_AddScienceFictionGenre_GenreSetContainsShienceFiction()
+        {
+            // Arrange
 
-        
+            var sut = new BookManagerProject.BookManager();
+
+            //Act
+
+            sut.AddGenre("Science Fiction");
+            //Assert
+
+            Assert.Contains("Science Fiction", sut.GetGenres());
+        }
+
+        [Fact]
+        public void RemoveGenre_AddGenreMysteryThenDeleteThem_GenresSetDoesntContainsMysteryGenre()
+        {
+            //Arrange
+            var sut = new BookManagerProject.BookManager();
+
+            //Act
+            sut.AddGenre("Mystery");
+            sut.RemoveGenre("Mystery");
+
+            //Assert
+            Assert.DoesNotContain("Mystery", sut.GetGenres());
+        }
+
+        [Fact]
+        public void AddBook_AddABook_BookAddedToList()
+        {
+            //Arrange
+            var sut = new BookManagerProject.BookManager();
+            var book = new Book("The Hobbit", "J.R.R. Tolkien", "Fantasy");
+            List<Book> expectedBooksList = new List<Book> { book };
+
+            //Act
+            sut.AddBook(book);
+            List<Book> actualBooksList = sut.GetBooks();
+
+            //Assert
+            Assert.Equal(expectedBooksList, actualBooksList);
+            Assert.Contains(book, sut.GetBooks());
+
+        }
+
 
     }
     
